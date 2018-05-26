6f27f1c7f0063713fcf086c410fcb8ec74adcffa
橋本長寿 <nagatoshi@hashimotochouju-no-MacBook-Pro.local>
2018-05-26 17:47:28 +0900

    aaa

---
+++ Assets/Tank.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+
+    //balletのプレハブ
+    public GameObject ballet;
+
+    public GameObject canon;
+    public Rigidbody shaft;
+    public Transform wheelLeft;
+    public Transform wheelRight;
+
+    public Vector3 wheelVelocityVec;
+
+    //from DNA
+    public float wheelsizeL = 1f;
+    public float wheelsizeR = 1f;
+    public float wheelVelocity;
+
+    public float balletVelocity = 100;
+    public float balletInterval = 3;
+    public float balletSize = 1;
+
+    public Color baseColor;
+    public Color wheelColoer;
+
+    //
+    public int killCount = 0;
+
+    //
+    private float counter;
+
+
+	// Use this for initialization
+	void Start () {
+
+        //DNA設定
+        wheelLeft.transform.localScale = new Vector3(
+            wheelLeft.transform.localScale.x,
+            wheelLeft.transform.localScale.y * wheelsizeL,
+            wheelLeft.transform.localScale.z * wheelsizeL
+        );
+
+        wheelLeft.transform.localScale = new Vector3(
+            wheelRight.transform.localScale.x,
+            wheelRight.transform.localScale.y * wheelsizeR,
+            wheelRight.transform.localScale.z * wheelsizeR
+        );
+
+        wheelVelocityVec = -transform.forward * wheelVelocity;
+
+        canon.GetComponent<MeshRenderer>().material.color = baseColor;
+        //wheelLeft.GetComponent<Material>().color = wheelColoer;
+
+        wheelLeft.GetComponent<MeshRenderer>().material.SetColor("_Color", wheelColoer);
+        wheelRight.GetComponent<MeshRenderer>().material.color = wheelColoer;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter += Time.deltaTime;
+        if(counter > balletInterval) {
+            counter = 0;
+            shoot();
+        }
+
+	}
+
+    void FixedUpdate()
+    {
+
+        shaft.AddRelativeTorque(wheelVelocityVec * Time.fixedDeltaTime);
+    }
+
+    public void shoot() {
+        GameObject b = Instantiate<GameObject>(ballet, transform.position + transform.forward, Quaternion.identity);
+        b.transform.localScale *= balletSize;
+        b.GetComponent<Rigidbody>().AddForce(transform.forward * balletVelocity);
+        b.GetComponent<Ballet>().tank = this;
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Ballet b = collision.gameObject.GetComponent<Ballet>();
+        if(b != null) {
+            b.tank.OnKill();
+            Dead();
+        }
+    }
+
+    public void Dead() {
+        Destroy(gameObject);
+    }
+
+
+    public void OnKill() {
+        killCount++;
+    }
+}
---
+++ Assets/TankManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankManager : MonoBehaviour {
+
+    public GameObject tankPrefab;
+    public authSample auth;
+
+    public async void CreateTank(){
+        GameObject tankObj = Instantiate<GameObject>(tankPrefab, Vector3.zero, Quaternion.identity);
+        Tank tank = tankObj.GetComponent<Tank>();
+        authSample.TankDna dna = await auth.CreateTank();
+
+        Debug.Log(dna.wheelLeft);
+        tank.wheelsizeL = dna.wheelLeft;
+        tank.wheelsizeR = dna.wheelRight;
+        tank.wheelVelocity = dna.cannonSpeed;
+        tank.balletSize = dna.bulletSize;
+        tank.balletVelocity = dna.speed;
+        tank.balletInterval = dna.interval;
+        tank.baseColor = dna.baseColor;
+        tank.wheelColoer = dna.wheelColor;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
